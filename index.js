28301a0e188df72d86556b280d3fd03f53fc970f
Syed <xayanmay@gmail.com>
2025-11-17 10:01:25 -0500

    Almost done

--- index.js
+++ index.js
@@ -1,16 +1,53 @@
 /**
  * Task Class
- * 
+ *
  * A class to manage tasks with title, priority, completion status, and time tracking.
  */
 
 class Task {
- //write your code here
- 
+  //write your code here
+  #completed;
+  #minuteSpent;
+  static allTasks = [];
+  constructor(title, priority) {
+    this.title = title;
+    this.priority = priority
+    this.#completed = false;
+    this.#minuteSpent = 0;
+  }
+  get completed() {
+    return this.#completed;
+  }
+  get minuteSpent() {
+    return this.#minuteSpent;
+  }
+
+  workOn(minute) {
+    this.#minuteSpent += minute;
+    console.log(
+      `Worked on ${this.title} for ${minute} minutes. Total time: ${
+        this.#minuteSpent
+      }`
+    );
+  }
+  complete() {
+    this.#completed = true;
+    console.log(`${this.title} has been completed!`);
+  }
+  isComplete() {
+    return this.#completed;
+  }
+
+  static getTotalTasks() {
+    return this.allTasks.length
+  }
+  static findByTitle(title) {
+    return this.allTasks
+  }
 }
 
 //Test class with examples below:
 
-
 // Export the Task class for testing
 module.exports = { Task };
+c;
